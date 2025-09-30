f1fdbd394c8e962f0668448031fddee26aef0aab
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-09-30 21:39:47 +0900

    Update release.sh
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- release.sh
+++ release.sh
@@ -7,6 +7,6 @@ cd ${SCRIPT_DIR}
 # Copy from Dev environment
 cp -rf ${DEV_DIR}/* -t ${SCRIPT_DIR}
 # Remove/Resotre files
-rm -rf Gemfile* local_debug.sh setup_ruby_ubuntu.sh _site
+rm -rf Gemfile* local_debug.sh setup_ruby_ubuntu.sh _site _dev
 git restore README.md
 
