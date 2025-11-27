8bf12234b9d61f15ef2b871936ca259fcb9c7398
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-11-27 15:06:05 +0900

    DEV: release.sh: Add protect to prevent except main branch
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- release.sh
+++ release.sh
@@ -4,6 +4,21 @@ SCRIPT_DIR=$(cd `dirname $0` && pwd)
 DEV_DIR=$SCRIPT_DIR/../rcar-community-board
 cd ${SCRIPT_DIR}
 
+print_err() {
+    for arr in "$@"; do
+        echo -e "\e[31m$arr\e[m"
+    done
+}
+
+# Check development repository branch
+DEV_BRANCH=$(git -C ${DEV_DIR} branch | grep '* ' | awk '{print $2}')
+if [[ "${DEV_BRANCH}" != "main" ]]; then
+    print_err "ERROR: ${DEV_DIR} is not main branch.($DEV_BRANCH}"
+    echo      ""
+    print_err "Please change branch to main and rerun this script"
+    exit -1
+fi
+
 # Copy from Dev environment
 cp -rf ${DEV_DIR}/* -t ${SCRIPT_DIR}
 # Remove/Resotre files
